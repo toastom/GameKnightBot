20db6b504ab0929b73432fa63d95c30ed79bdb77
Kirbae <gerben.telefoon@gmail.com>
2019-06-24 22:30:29 +0200

    fixed up some code, added creds.json to gitignore

--- bot.py
+++ bot.py
@@ -1,10 +1,10 @@
-<<<<<<< Updated upstream
+
 import os
 import discord
 import random
-=======
+
 import os, discord, random, sys
->>>>>>> Stashed changes
+
 from discord.ext import commands
 from discord.utils import get
 import gspread
@@ -29,18 +29,18 @@ async def on_ready():
     print('Logged in as')
     print(bot.user.name)
     print(bot.user.id)
-<<<<<<< Updated upstream
-=======
+
+
     print('------')
->>>>>>> Stashed changes
+
     servers = list(bot.guilds)
     print('Connected on '+ str(len(servers)) + ' servers:')
     print('\n'.join('>'+ server.name for server in servers))
     print('------')
 
-<<<<<<< Updated upstream
-bot.run(TOKEN)
-=======
+
+
+
 @bot.command(name="addgame")
 async def add_game(ctx, game):
     spread = client.open("Testing")
@@ -49,11 +49,11 @@ async def add_game(ctx, game):
     except gspread.exceptions.APIError:
         print("A(n) {} error occurred.".format(sys.exc_info()[0]))
         await ctx.message.channel.send("Game is already added.")
-        client.close()
+        
         return
 
     await ctx.message.channel.send("New game {} added".format(str(game)))
-    client.close()
+
 
 bot.run(TOKEN)
->>>>>>> Stashed changes
+
