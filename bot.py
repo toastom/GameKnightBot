4ca6b08f9bd35befc0d13f2ad06a6078671e27d9
Kirbae_ <35197729+Kirbae00@users.noreply.github.com>
2019-06-25 09:39:39 +0200

    Update bot.py
    updated error catching on adding spreadsheet when joining

--- bot.py
+++ bot.py
@@ -46,11 +46,16 @@ async def on_guild_join(guild):
             'developerMetadata': guild.id
         }
     }
-    spreadsheet = client.create(guild.name)
-    spreadsheet.share('gameknightbot@gmail.com', perm_type='user', role='writer')
+    try:
+        spreadsheet = client.create(guild.name)
+        spreadsheet.share('gameknightbot@gmail.com', perm_type='user', role='writer')
+        general = find(lambda x: x.name == 'general', guild.text_channels)
+        if general and general.permissions_for(guild.me).send_messages:
+                await general.send('Hello a Spreadsheet with the name `{}` has been created for your server'.format(str(guild.name)))
+    except gspread.exceptions.APIError:
     general = find(lambda x: x.name == 'general', guild.text_channels)
     if general and general.permissions_for(guild.me).send_messages:
-        await general.send('Hello a Spreadsheet with the name `{}` has been created for your server'.format(str(guild.name)))
+        await general.send('Hello a Spreadsheet with the name `{}` couldnt be created')
 
 
 
